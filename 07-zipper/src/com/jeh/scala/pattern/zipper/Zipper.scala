f25fed55c913d33e9b58597340ea0a3a8679c2db
John Hunt <john.hunt10@gmail.com>
2020-06-22 15:49:30 +0100

    Added additional comments

--- 07-zipper/src/com/jeh/scala/pattern/zipper/Zipper.scala
+++ 07-zipper/src/com/jeh/scala/pattern/zipper/Zipper.scala
@@ -1,19 +1,54 @@
 package com.jeh.scala.pattern.zipper
 
+/**
+ * Zippers support the traversal and possible mutation
+ * of immutable data structures such as trees
+ *
+ * @tparam A
+ */
 trait Zipper[A] {
-  val focus: A
-  val left: Seq[A]
-  val right: Seq[A]
 
+  // Import methods in Zipper object
+  import Zipper._
+
+  val focus: A // a particular node in a structure
+  val left: Seq[A] // Sequence of elements to the left of the focus point
+  val right: Seq[A] // Sequence of elements to the right of the focus point
+
+  /**
+   * Move the focus point to the next element
+   * The exact semantics of this differs depending upon the underlying
+   * data structure - e.g. a list versus a tree
+   *
+   * @return
+   */
   def next: Option[Zipper[A]] = right match {
     case Nil => None
     case _ => Some(zipper(left :+ focus, right.head, right.tail))
   }
+
+  /**
+   * Move the focus point to the next element
+   */
   def prev: Option[Zipper[A]] = left match {
     case Nil => None
     case _ => Some(zipper(left.init, left.last, focus +: right))
   }
+
+  /**
+   * Insert a new element at the focus point.
+   * This Zipper is immutable and this a new Zipper is created.
+   *
+   * @param a element to be inserted
+   * @return a New Zipper
+   */
   def insert(a: A): Zipper[A] = zipper(left :+ focus, a, right)
+
+  /**
+   * Delete an element form the Zipper - return a new Zipper instance
+   *
+   * @return
+   */
   def delete: Option[Zipper[A]] = right match {
     case Nil => None
     case _ => Some(left match {
@@ -23,14 +58,27 @@ trait Zipper[A] {
   }
 
   def atStart: Boolean = left.isEmpty
+
   def atEnd: Boolean = right.isEmpty
-  
-  override def toString = "Zipper: " + focus + " (left: " + left + ") (right: " + right + ")" 
+
+  override def toString = "Zipper: " + focus + " (left: " + left + ") (right: " + right + ")"
+}
+
+object Zipper {
+  // Convenience method used to create a Zipper instance
+  def zipper[A](l: Seq[A], f: A, r: Seq[A]): Zipper[A] = new Zipper[A] {
+    val focus = f
+    val left = l
+    val right = r
+  }
+
 }
 
 object Test extends App {
+  import Zipper._
+
   val data = Seq("John", "Denise", "Phoebe", "Adam")
-  var zip = zipper[String](Nil, data.head, data.tail)
+  var zip = zipper[String](l = Nil, f = data.head, r = data.tail)
   println(zip)
   println(zip.atStart)
   zip = zip.next.get
